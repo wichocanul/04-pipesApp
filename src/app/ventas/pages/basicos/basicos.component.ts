cf6f8de8d6abf15e629117d2877f02a085bb155c
Wicho Canul <wichocanul07@gmail.com>
2022-11-08 01:06:02 -0600

    date pipe

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent{
 
-  constructor() { }
+  nombreLower: string = 'wicho';
+  nombreUpper: string = 'WICHO';
+  nombreCompleto: string = 'wIchOcaNul';
 
-  ngOnInit(): void {
-  }
+  fecha: Date = new Date();
 
 }
